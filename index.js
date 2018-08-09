dd5fbd703542e24fbaafbe06f4c219c852cecc0b
Dimitar Nanov <dimitar.nanov@ecollect.net>
2018-08-09 08:41:30 +0300

    module-alias setup

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const moduleAlias = require('module-alias');
+const path = require('path');
+
 const Oblak = require('./lib/Oblak');
+
+moduleAlias.addAlias('lib', path.join(process.cwd(), 'lib'));
+moduleAlias.addAlias('oblak-data', path.join(process.cwd(), 'oblak-data.js'));
 
 module.exports = Oblak;
